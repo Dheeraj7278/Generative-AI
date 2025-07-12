c608da8a76282db78f98255161f4f9e4a84edb1b
Dheeraj7278 <93006721+Dheeraj7278@users.noreply.github.com>
2025-07-12 21:54:06 +0530

    Add files via upload

---
+++ 15.CREWAI/tools.py
@@ -0,0 +1,5 @@
+from crewai_tools import YoutubeChannelSearchTool
+
+#Initialize the tool with a specific youtube channel handle to target your search
+
+yt_tool=YoutubeChannelSearchTool(Youtube_channel_handler="@krishnaik06")
